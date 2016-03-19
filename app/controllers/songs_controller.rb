56b782819fdb6fadb6d43150d16516075beafef0
azheng249 <andy.zheng249@gmail.com>
2016-03-18 23:08:44 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,14 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    # Artist not found
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      redirect_to artists_path
+
+    # New song
+    else
+      @song = Song.new(artist_id: params[:artist_id])
+    end
   end
 
   def create
@@ -39,7 +46,21 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      artist = Artist.find_by(id: params[:artist_id])
+
+      # Artist not found. Redirect to artists path
+      if artist.nil?
+        redirect_to artists_path, alert: "Artist not found"
+
+      # Artist found. Edit song or redirect to artist's songs path if song not found.
+      else
+        @song = artist.songs.find_by(id: params[:id])
+        redirect_to artist_songs_path(artist), alert: "Song not found"
+      end
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
   def update
@@ -64,7 +85,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_id)
   end
 end
 
